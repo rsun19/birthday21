fcf3fd029483e018b9a7c5d040c605bb6c26c592
rsun19 <robertssun1234@gmail.com>
2023-12-20 02:47:10 -0500

    added github pages

---
+++ src/components/FreqQuestions.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+interface question {
+    id: string;
+    question: string;
+    answer: string;
+}
+
+const FreqQuestions = () => {
+    const questions: question[] = [
+        {
+            id: "1",
+            question: "Do you think we have a good lineup?",
+            answer: "Idk. I never drank alcohol before the age of 21."
+        }, 
+        {
+            id: "2",
+            question: "Will the party be lit af?",
+            answer: "yes"
+        },
+        {
+            id: "3",
+            question: "How long did this website take you to make",
+            answer: "1 hour"
+        }
+    ]
+
+    return (
+        <div style={{ marginTop: '8rem', display: 'flex', alignContent: 'center', flexDirection: 'column', alignItems: 'center', textAlign: 'center'}}>
+            {questions.map((item) => (
+                <div id={item.id}>
+                    <h3>
+                        {item.id}. {item.question}
+                    </h3>
+                    <h3>
+                        {item.answer}
+                    </h3>
+                </div>
+            ))}
+            <br />
+            <br />
+        </div>
+    );
+}
+
+export default FreqQuestions;
+
---
+++ src/components/Home.tsx
@@ -0,0 +1,113 @@
+import React, { useEffect } from 'react';
+import birthday from '.././assets/birthday.jpg';
+import { Variants, motion } from "framer-motion";
+// eslint-disable-next-line
+// @ts-ignore comment
+import video from ".././assets/masterpiece.mp4";
+import FreqQuestions from './FreqQuestions';
+
+const Home = () => {
+
+    useEffect(() => {
+        const scrollToTop = () => {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        };
+    
+        // Scroll to top on component mount
+        scrollToTop();
+      }, []);
+
+    interface Props {
+        emoji: string;
+        hueA: number;
+        hueB: number;
+      }
+      
+    const cardVariants: Variants = {
+    offscreen: {
+        y: 300
+    },
+    onscreen: {
+        y: 50,
+        rotate: -10,
+        transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8
+        }
+    }
+    };
+    
+    const hue = (h: number) => `hsl(${h}, 100%, 50%)`;
+    
+    function Card({ emoji, hueA, hueB }: Props) {
+    const background = `linear-gradient(306deg, ${hue(hueA)}, ${hue(hueB)})`;
+    
+    return (
+        <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+        >
+        <div className="splash" style={{ background }} />
+        <motion.div className="card" variants={cardVariants}>
+            {emoji}
+        </motion.div>
+        </motion.div>
+    );
+    }
+    
+    const food: [string, number, number][] = [
+    ["Trulys", 340, 10],
+    ["Suju???? maybe", 20, 40],
+    ["Pink Whitney", 60, 90],
+    ["Alcohol", 80, 120],
+    ["Alcohol", 100, 140],
+    ["Alcohol", 205, 245],
+    ];
+
+    return (
+        <div>
+            <motion.div
+            style={{ display: 'flex', justifyContent: 'center' }}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 2,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01]
+            }}
+            >
+                <div style={{
+                     marginTop: '3rem',display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+                    <h3>
+                        You're invited to my 21st Birthday Party! Yay!
+                    </h3>
+                    <h4>
+                        Insert date
+                    </h4>
+                    <br />
+                    <video autoPlay muted controls style={{ height: '50vh' }}>
+                        <source src={video}
+                        type="video/mp4" />
+                    </video>
+                    <h4>put Javkson's address here</h4>
+                    <h3> Frequently Asked Questions:</h3>
+                    <h3>Lineup:</h3>
+                    <div>
+                    {food.map(([emoji, hueA, hueB]) => (
+                        <Card emoji={emoji} hueA={hueA} hueB={hueB} key={emoji} />
+                    ))}
+                    </div>
+                    <FreqQuestions />
+                </div>
+            </motion.div>
+        </div>
+    );
+}
+
+export default Home;
